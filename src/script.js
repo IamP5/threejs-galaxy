2fa9bc129c26a8b81daf96813cae7043c72a9593
Tuba <brunogabrielsd@hotmail.com>
2023-12-24 17:52:22 -0300

    feat: galaxy pattern with particles

--- src/script.js
+++ src/script.js
@@ -14,14 +14,135 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Test cube
+/** 
+ * Galaxy
  */
-const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
-)
-scene.add(cube)
+const parameters = {
+    count: 75000,
+    size: 0.01,
+    radius: 4,
+    branches: 8,
+    spin: 1,
+    randomness: 0.4,
+    randomnessPower: 4,
+    insideColor: '#ff6030',
+    outsideColor: '#1b3984'
+}
+
+
+let geometry = null
+let material = null
+let points = null
+
+const generateGalaxy = () => {
+    // Destroy old galaxy
+    if(points !== null) {
+        geometry.dispose()
+        material.dispose()
+        scene.remove(points)
+    }
+
+    /**
+     * Geometry
+     */
+    geometry = new THREE.BufferGeometry()
+
+    const colorInside = new THREE.Color(parameters.insideColor)
+    const colorOutside = new THREE.Color(parameters.outsideColor)
+
+    const positions = new Float32Array(parameters.count * 3)
+    const colors = new Float32Array(parameters.count * 3)
+
+    for(let i = 0; i < parameters.count; i++) {
+        const i3 = i * 3
+
+        const radius = Math.random() * parameters.radius
+
+        const mixedColor = colorInside.clone()
+        mixedColor.lerp(colorOutside, radius / parameters.radius)
+    
+        const spinAngle = radius * parameters.spin
+        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
+        
+        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
+    
+        positions[i3    ] = Math.cos(branchAngle + spinAngle) * radius + randomX
+        positions[i3 + 1] = randomY
+        positions[i3 + 2] = Math.sin(branchAngle + spinAngle) * radius + randomZ
+
+        colors[i3    ] = mixedColor.r
+        colors[i3 + 1] = mixedColor.g
+        colors[i3 + 2] = mixedColor.b
+    }
+
+    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
+
+    /**
+     * Material
+     */
+    material = new THREE.PointsMaterial({
+        size: parameters.size,
+        sizeAttenuation: true,
+        depthWrite: false,
+        blending: THREE.AdditiveBlending,
+        vertexColors: true
+    })
+
+    /**
+     * Points
+     */
+    points = new THREE.Points(geometry, material)
+    scene.add(points)
+}
+generateGalaxy()
+
+gui.add(parameters, 'count')
+    .min(100)
+    .max(100000)
+    .step(100)
+    .onChange(generateGalaxy)
+
+gui.add(parameters, 'size')
+    .min(0.001)
+    .max(0.1)
+    .step(0.001)
+    .onChange(generateGalaxy)
+
+gui.add(parameters, 'radius')
+    .min(0.01)
+    .max(20)
+    .step(0.01)
+    .onChange(generateGalaxy)
+
+gui.add(parameters, 'branches')
+    .min(2)
+    .max(20)
+    .step(1)
+    .onChange(generateGalaxy)
+
+gui.add(parameters, 'spin')
+    .min(-5)
+    .max(5)
+    .step(0.001)
+    .onChange(generateGalaxy)
+
+gui.add(parameters, 'randomness')
+    .min(0)
+    .max(2)
+    .step(0.001)
+    .onChange(generateGalaxy)
+
+gui.add(parameters, 'randomnessPower')
+    .min(1)
+    .max(10)
+    .step(0.001)
+    .onChange(generateGalaxy)
+
+gui.addColor(parameters, 'insideColor').onChange(generateGalaxy)
+gui.addColor(parameters, 'outsideColor').onChange(generateGalaxy)
 
 /**
  * Sizes
@@ -51,7 +172,7 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
+camera.position.x = 2
 camera.position.y = 3
 camera.position.z = 3
 scene.add(camera)
@@ -77,6 +198,8 @@ const clock = new THREE.Clock()
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+
+    points.rotation.y = Math.sin(Math.PI * 0.25) * 0.5 * elapsedTime
 
     // Update controls
     controls.update()
